fa67d470b702a8df064f7060360a3285c2295ee4
quevedolevi <quevedolevi@gmail.com>
2022-06-19 21:12:40 -0400

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//we check directory relative to the file
+import { App } from './components/App';
 
+//Creates a root element in the DOM
+//React DOM library is used to interact with the DOM
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
@@ -12,8 +14,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
